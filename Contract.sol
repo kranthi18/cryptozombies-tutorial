2fd8751f37e433a1f4b524a756dd5368de34c406
kranthigy <kranthi@goodyear.com>
2021-12-21 08:14:22 -0500

    Added events to be fired off when we create a new zombie.

--- Contract.sol
+++ Contract.sol
@@ -1,6 +1,10 @@
 pragma solidity >=0.5.0 <0.6.0;
 
 contract ZombieFactory {
+
+    // Events
+    event NewZombie(uint zombieId, string name, uint dna);
+
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
 
@@ -12,7 +16,8 @@ contract ZombieFactory {
     Zombie[] public zombies;
 
     function _createZombie(string memory _name, uint _dna) private {
-        zombies.push(Zombie(_name, _dna));
+        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        emit NewZombie(id, _name, _dna);
     }
 
     function _generateRandomDna(string memory _str) private view returns (uint) {
