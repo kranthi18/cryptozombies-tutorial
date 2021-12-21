ee345e36c1ee10107b3b189fb430ca70dfe3fe94
kranthigy <kranthi@goodyear.com>
2021-12-21 10:46:43 -0500

    Added ownerOf and balanceOf implementation based on ECR721 contract.

--- zombieownership.sol
+++ zombieownership.sol
@@ -1,7 +1,25 @@
 pragma solidity >=0.5.0 <0.6.0;
 
 import "./zombieattack.sol";
+import "./erc721.sol";
 
-contract ZombieOwnership is ZombieAttack {
+contract ZombieOwnership is ZombieAttack, ERC721{
 
+    function balanceOf(address _owner) external view returns (uint256) {
+        // Return the number of zombies _owner has here
+        return ownerZombieCount[_owner];
+    }
+
+    function ownerOf(uint256 _tokenId) external view returns (address) {
+        // Return the owner of _tokenId here
+        return zombieToOwner[_tokenId];
+    }
+
+    function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
+
+    }
+
+    function approve(address _approved, uint256 _tokenId) external payable {
+
+    }
 }
